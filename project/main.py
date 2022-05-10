a69bda7d38b7e11826f546207bf844aaf68d26bf
Jose Gregorio <jose.gregorio.ca888@gmail.com>
2022-05-09 21:06:22 -0400

    Replace name of def

--- project/main.py
+++ project/main.py
@@ -1,19 +1,18 @@
 from requests_html import HTMLSession
 
-
 session = HTMLSession()
 
 
-def get_planzuares_products():
-    url_planzuares = "https://tucentralonline.com/Plaza-Las-Americas-03/tienda/"
-    r = session.get(url_planzuares)
+def get_central_products():
+    url_central = "https://tucentralonline.com/Plaza-Las-Americas-03/tienda/"
+    r = session.get(url_central)
 
     # r.html.find('li.product-col:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(2) > h3:nth-child(1)')
 
     return r
 
 
-print(get_planzuares_products())
+print(get_central_products())
 
 
 
