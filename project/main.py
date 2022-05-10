a98eb28bd391aea63a098928c0e05ffd322e3031
Jose Gregorio <jose.gregorio.ca888@gmail.com>
2022-05-10 17:19:10 -0400

    Add regex in product request to split ID of product and create new fuction which get from page price of product use regex again

--- project/main.py
+++ project/main.py
@@ -1,16 +1,33 @@
 from requests_html import HTMLSession
+import re 
 
 session = HTMLSession()
+url = "https://gamaenlinea.com/ALIMENTOS-FRESCOS/L%C3%A1cteos/Mantequilla-Margarina/MARGARINA-CON-SAL-NELLY-500-GR/p/10003343"
+r = session.get(url)
 
-
-def get_excelsior_gamma_data():
-    url = "https://gamaenlinea.com/ALIMENTOS-FRESCOS/L%C3%A1cteos/Mantequilla-Margarina/MARGARINA-CON-SAL-NELLY-500-GR/p/10003343"
-
-    r = session.get(url)
-    product = r.html.find(".name", first=True).text
+def get_excelsior_gamma_name():
+    # Url = ""
     
+    product = r.html.find(".name", first=True).text
+    product = product.lower()
+    product = re.split('id[a-z0-9]*', product)[0]
+
     return product
 
-get_excelsior_gamma_data()
+
+def get_excelsior_gamma_price():
+    # url = ""
+
+    price = r.html.find(".from-price-value", first=True).text
+    price = re.split('Total Ref.[0-9.]*', price)[1]
+
+    return price
 
 
+def main():
+    price = get_excelsior_gamma_price()
+    product_name = get_excelsior_gamma_name()
+    print(f"{product_name}: ${price}")
+
+
+main()
