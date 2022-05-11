6af0abedb8d3ee90ceeada5a7f84d9172f886d07
Jose Gregorio <jose.gregorio.ca888@gmail.com>
2022-05-11 19:10:29 -0400

    Edit excelsior.py
    Edit excelsior.py in new branch, with bs4 get all href tags in url (gama en linea)

--- project/excelsior.py
+++ project/excelsior.py
@@ -1,10 +1,30 @@
-from requests_html import HTMLSession
-
-session = HTMLSession()
+#! /bin/python3
+from bs4 import BeautifulSoup
+import urllib.request
+import re 
 
 url = "https://gamaenlinea.com/VIVERES/c/001?q=%3Arelevance&page=0"
+url = urllib.request.urlopen(url).read().decode()
 
-r = session.get(url)
 
-print(r.html.links)
+def get_href_tags():
+    soup = BeautifulSoup(url, features="lxml")
+    tags = soup("a")
+    links = []
 
+    for tag in tags:
+        tag = tag.get('href')
+        links.append(tag)
+        
+    
+    for link in links:
+        try:
+            print(re.findall('/VIVERES/[a-zA-Z-]*/[a-zA-Z-]*/[A-Za-z0-9-]*.*', link))
+        except TypeError:
+            print("NoneType")
+    
+
+    
+
+
+get_href_tags()
