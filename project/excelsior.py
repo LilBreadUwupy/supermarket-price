690b3bc5cefcd0edf761745b731e47508816bcf2
Jose Gregorio <jose.gregorio.ca888@gmail.com>
2022-05-11 19:58:22 -0400

    In Excelsior iterar urls of "viveres" in for in range (62)
    Create main fuction to run get_href_tags functions and url fuctions

--- project/excelsior.py
+++ project/excelsior.py
@@ -1,30 +1,42 @@
 #! /bin/python3
+
 from bs4 import BeautifulSoup
 import urllib.request
 import re 
 
-url = "https://gamaenlinea.com/VIVERES/c/001?q=%3Arelevance&page=0"
-url = urllib.request.urlopen(url).read().decode()
+
+def iterar_url(n):
+    url = "https://gamaenlinea.com/VIVERES/c/001?q=%3Arelevance&page={}".format(n)
+    return url 
 
 
-def get_href_tags():
+def get_href_tags(url):
+    url = urllib.request.urlopen(url).read().decode()
     soup = BeautifulSoup(url, features="lxml")
     tags = soup("a")
     links = []
-
+    urls = []
     for tag in tags:
         tag = tag.get('href')
         links.append(tag)
-        
     
     for link in links:
         try:
-            print(re.findall('/VIVERES/[a-zA-Z-]*/[a-zA-Z-]*/[A-Za-z0-9-]*.*', link))
+            link = re.findall('/VIVERES/[a-zA-Z-]*/[a-zA-Z-]*/[A-Za-z0-9-]*.*', link)
+            if link:
+                print(link)
+                urls.append(link)
         except TypeError:
-            print("NoneType")
-    
+            pass
 
-    
+    # for url in urls:
+    #     url = 'https://gamaenlinea.com' + url
+    #     return url 
 
+def main():
+    for n in range(62):
+        url = iterar_url(n)
+        get_href_tags(url)
+    print("Todos los links han sido obtenidos")
 
-get_href_tags()
+main()
