bbc562452264b80fa291f49a8e7fc77161d063c0
Jose Gregorio <jose.gregorio.ca888@gmail.com>
2022-05-12 19:24:47 -0400

    Lots of tweaks - Day 4
    README.MD - Add project title
    
    test - Create folder for tests
    
    excelsior.py -
    
    Edit get_url - pass category, category number and number parameters, with the format method append to the url
    
    Edit get_href_tags -
    add exception handling on rejection by web (No specific error found - change later), all exception handling inside a while
    
    change regex to get a str depending on category
    
    create file function -
    create a file in the test folder with the name of the category
    
    Create write_file -
    receive file_data and urls, open a file and write received urls
    
    create list-of-page-to-range
    receives category and categories depending on the category determines how many pages to scrape
    
    edit main function
    
    add variables with categories and their respective numbers
    
    add for for categories
    
    in the for of categories messages are printed for the user, createfile is called passing the category, the number of pages is obtained by calling list_of_page__to_range as r
    starts a for in range r: where get url receives its parameters, and the links are passed one by one to write_file so that it writes them to the corresponding file
    
    Finally, the confirmation of the success of the program is printed

--- project/excelsior.py
+++ project/excelsior.py
@@ -1,42 +1,122 @@
 #! /bin/python3
 
+from fileinput import filename
+from unicodedata import category
 from bs4 import BeautifulSoup
+from time import sleep 
 import urllib.request
 import re 
 
 
-def get_url(n):
-    url = "https://gamaenlinea.com/VIVERES/c/001?q=%3Arelevance&page={}".format(n)
-    return url 
+def get_url(category, category_number, n):
+    url = "https://gamaenlinea.com/{}/c/{}?q=%3Arelevance&page={}".format(category, category_number, n)
+    
+    return url
 
 
-def get_href_tags(url):
-    url = urllib.request.urlopen(url).read().decode()
-    soup = BeautifulSoup(url, features="lxml")
-    tags = soup("a")
+def get_href_tags(url, category):
+    connection = False
+    while not connection:
+        try:
+            url = urllib.request.urlopen(url).read().decode()
+            soup = BeautifulSoup(url, features="lxml")
+            tags = soup("a")
+            connection = True
+        except:
+            print("Error conexión no segura, reintentando en 20 segundos")
+            sleep(20)
+
     links = []
-    urls = []
+
     for tag in tags:
         tag = tag.get('href')
         links.append(tag)
     
+    urls = []
+
     for link in links:
         try:
-            link = re.findall('/VIVERES/[a-zA-Z-]*/[a-zA-Z-]*/[A-Za-z0-9-]*.*', link)
+            link = re.findall(f'/{category}/[a-zA-Z-]*/[a-zA-Z-]*/[A-Za-z0-9-]*.*', link)
             if link:
-                print(link)
                 urls.append(link)
         except TypeError:
             pass
 
-    # for url in urls:
-    #     url = 'https://gamaenlinea.com' + url
-    #     return url 
+    links.clear()
+
+    for url in urls:
+        url = 'https://gamaenlinea.com' + str(url)
+        url = url.replace('[', '').replace("'", "").replace("]", "")
+        links.append(url)
+        
+    return links
+
+
+def create_file(category):
+    file_directory = "/home/lilbreaduwu/Documentos/proyectos/projectfolder/test/"
+    file_name = f"Excelsior-gamma-{category.lower()}.txt"
+    file_data = file_directory + file_name
+    file = open(file_directory + file_name, "a")
+    file.write(f"Lista de enlaces en la categoría {category.lower()}")
+
+    return file_data
+
+
+def write_file(file_data, urls):
+    file = open(file_data, "a")
+    file.write(urls + "\n")
+
+
+def list_of_page_to_range(category, categories):
+
+    if category == categories[0]:
+        r = 62
+    elif category == categories[1]:
+        r = 22
+    elif category == categories[2]:
+        r = 29
+    elif category == categories[3]:
+        r = 19
+    elif category == categories[4]:
+        r = 11
+    elif category == categories[5]:
+        r = 7
+    elif category == categories[6]:
+        r = 3
+    elif category == categories[7]:
+        r = 4
+    else:
+        r = 6
+        
+    return r 
+
 
 def main():
-    for n in range(62):
-        url = get_url(n)
-        get_href_tags(url)
+    categories = ["VIVERES", "ALIMENTOS-FRESCOS", "BEBIDAS", "CUIDADO-PERSONAL", "LIMPIEZA", "HOGAR", "MASCOTAS", "OCASION", "CUIDADO-DE-LA-SALUD"]
+    category_numbers = ["001", "002", "003", "004", "005", "006", "007", "008", "009"]
+    index_category = 0
+
+    for category in categories:
+
+        print(f"Empezando a obtener enlaces de la categoría {category.lower()}...")
+        sleep(5)
+        file_data = create_file(category)
+
+        category_number = category_numbers[index_category]
+        r = list_of_page_to_range(category, categories)
+        for n in range(r):
+            
+            url = get_url(category, category_number, n)
+            links = get_href_tags(url, category)
+
+            print(f"Obteniendo enlaces de {url}... ({n+1}/{r})")
+
+            for link in links:
+                write_file(file_data, link)
+
+        index_category += 1
+
     print("Todos los links han sido obtenidos")
+
 
 main()
--- project/test.py
+++ project/test.py
@@ -1,2 +1,12 @@
-for n in range(62):
-    print(n)
+
+def get_url(n):
+    categories = ["VIVERES", "ALIMENTOS-FRESCOS", "BEBIDAS", "CUIDADO-PERSONAL", "LIMPIEZA", "HOGAR", "MASCOTAS", "OCASIÓN", "CUIDADO-DE-LA-SALUD"]
+    categories_num = ["001", "002", "003", "004", "005", "006", "007", "008", "009"]
+    for categorie in categories:
+        cn = 0
+        url = "https://gamaenlinea.com/{}/c/{}?q=%3Arelevance&page={}".format(categorie, categories_num[cn], n)
+        cn += 1
+        return url 
+
+
+print(get_url(10))
