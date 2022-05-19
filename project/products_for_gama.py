779de98fac60145123598eac28791a8aa44ffcd7
Jose Gregorio <jose.gregorio.ca888@gmail.com>
2022-05-19 13:53:51 -0400

    lot a changes in main proyect.
    Fix bug where file.pkl not list all link in big list, so product_for_gamma not get all links then not get all products
    Create script where get urls for plaza supermarket
    Delete test files

--- project/products_for_gama.py
+++ project/products_for_gama.py
@@ -6,8 +6,7 @@ from ssl import SSLCertVerificationError
 from urllib.error import URLError
 import re
 
-
-urls = create_file()
+list_urls = create_file()
 
 
 product_info = {
@@ -28,48 +27,47 @@ def get_price_and_name():
     id = 1
     lost_links = 0
     #Itera urls
-    for url in urls:
-        print(f'Obteniendo base de datos de Gama express ({id}/{len(urls)})')
-        new_product = product_info.copy()
-        new_product['Link'] = url.replace('\n', '')
-        new_product['ID'] = id
-        connection = False
+    for urls in list_urls:
+        for url in urls:
+            print(f'Obteniendo producto en {url}...({id}/{len(urls)})')
+            new_product = product_info.copy()
+            new_product['Link'] = url.replace('\n', '')
+            new_product['ID'] = id
+            connection = False
 
-        while not connection:
-            try:
-                url = urllib.request.urlopen(url).read().decode()
-                soup = BeautifulSoup(url, 'lxml')
-                connection = True
-            except SSLCertVerificationError:
-                print("SSLCertVerificationError: reintentando en 20 segundos")
-                sleep(20)
-            except URLError:
-                print("URLError: reintentando en 10 segundos")
-                sleep(10)
-                #lost_links += 1
+            while not connection:
+                try:
+                    url = urllib.request.urlopen(url).read().decode()
+                    soup = BeautifulSoup(url, 'lxml')
+                    connection = True
+                except SSLCertVerificationError:
+                    print("SSLCertVerificationError: reintentando en 20 segundos")
+                    sleep(20)
+                except URLError:
+                    print("URLError: reintentando en 10 segundos")
+                    sleep(10)
+                    #lost_links += 1
 
-       
-        # Get product with soup and cut with regex
-        product = soup.find("div", {"class":"name"})
-        product = str(re.findall('name">[A-ZÁÉÍÓÚ0-9a-záéíóúñÑ %-/]*<', str(product)))
-        new_product['Product'] = product.replace('[', '').replace("'", "").replace("]", "").replace('name">', "").replace('<', "").lower()
+            # Get product with soup and cut with regex
+            product = soup.find("div", {"class":"name"})
+            product = str(re.findall('name">[A-ZÁÉÍÓÚ0-9a-záéíóúñÑ %-/]*<', str(product)))
+            new_product['Product'] = product.replace('[', '').replace("'", "").replace("]", "").replace('name">', "").replace('<', "").lower()
 
-        #Get price with soup and cut with regex
-        price = soup.find("div", {"class":"from-price-value"})
-        price = str(re.findall('Total Ref. ([0-9,]*)', str(price)))
-        new_product["Price"] = price.replace('[', '').replace("'", "").replace("]", "")
+            #Get price with soup and cut with regex
+            price = soup.find("div", {"class":"from-price-value"})
+            price = str(re.findall('Total Ref. ([0-9,]*)', str(price)))
+            new_product["Price"] = price.replace('[', '').replace("'", "").replace("]", "")
 
-        id += 1
-        product_data['Gama express'] = new_product 
-        with open('Prueba.txt', 'a') as file:
-            file.write(str(new_product) + '\n')
+            id += 1
+            product_data['Gama express'] = new_product 
+            with open('Prueba2.txt', 'a') as file:
+                file.write(str(new_product) + '\n')
 
-        for r in range(1,11):
-            if str(id) == f'{r}00': 
-                print('Durmiendo 60 segundos antes de continuar')
-                sleep(60)
+            for r in range(1,11):
+                if str(id) == f'{r}00': 
+                    print('Durmiendo 60 segundos antes de continuar')
+                    sleep(60)
 
-    
     print(f"Base de datos obtenida, se perdieron{lost_links} durante el proceso creando archivo...")
     
 
--- project/urls_for_plaza.py
+++ project/urls_for_plaza.py
@@ -1,5 +1,7 @@
+from time import sleep
 from bs4 import BeautifulSoup
 import urllib.request
+from urllib.error import URLError
 import re
 
 
@@ -10,20 +12,57 @@ cat_number = [ '01', '03', '02', '06', '05', '04', '07', '08', '10']
 def get_url():
     links = []
     for n in cat_number:
+        i = 0
+        r = get_range(n)
+        for page in range(1,r):
+            url = f"https://www.elplazas.com/Products.php?cat={n}W&Page={page}"
+            print(f"Obteniendo enlaces de {url}... ({i+1}/{r-1})")
+            i += 1
+            connection = False
+            while not connection:
+                try:
+                    url = urllib.request.urlopen(url).read().decode()
+                    soup = BeautifulSoup(url, features='lxml')
+                    tags = soup('a')
+                    connection = True
+                except URLError:
+                    print('URlError: reintentando en 10 segundos')
+                    sleep(10)
+                    
+            for tag in tags:
+                tag = tag.get('href')
+                tag = re.findall('Product.php.code=[0-9]*&suc=[0-9]*', tag)
+                if tag:
+                    tag = 'https://www.elplazas.com/' + str(tag) + '\n'
+                    tag = tag.replace('[', '').replace("'", "").replace("]", "")
+                if tag not in links:
+                    links.append(tag)
+          
+    with open('Prueba-plaza.txt', 'a') as file:
+        file.write(str(links))
 
-        url = f"https://www.elplazas.com/Products.php?cat={n}W"
-        url = urllib.request.urlopen(url).read().decode()
-        soup = BeautifulSoup(url, features='lxml')
-        tags = soup('a')
-        for tag in tags:
-            tag = tag.get('href')
-            tag = re.findall('Product.php.code=[0-9]*&suc=[0-9]*', tag)
-            if tag:
-                tag = 'https://www.elplazas.com/' + str(tag)
-                tag = tag.replace('[', '').replace("'", "").replace("]", "")
-                print(tag)
-            if tag not in links:
-                links.append(tag)
+
+def get_range(n):
+    if n == cat_number[0]:
+        r = 17
+    elif n == cat_number[1]:
+        r = 212
+    elif n == cat_number[2]:
+        r = 34
+    elif n == cat_number[3]:
+        r = 47
+    elif n == cat_number[4]:
+        r = 39
+    elif n == cat_number[5]:
+        r = 66
+    elif n == cat_number[6]:
+        r = 10
+    elif n == cat_number[7]:
+        r = 26
+    else: 
+        r = 1
+
+    return r 
 
 
 get_url()
